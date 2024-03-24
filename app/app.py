593b37376fa65c6956b6de717e9b2a6ae1d88da3
TayebMoha <116709001+TayebMoha@users.noreply.github.com>
2024-03-24 23:08:31 +0100

    Update app.py

--- app/app.py
+++ app/app.py
@@ -4,7 +4,7 @@ import mysql.connector
 app = Flask(__name__)
 
 db_config = {
-    'host': '127.0.0.1',
+    'host': 'database-container',
     'user': 'user',
     'password': 'password',
     'database': 'appdb'
